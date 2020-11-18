03e1560f40c11f64249fc1f07509cb784322257e
碧空实验室 <32271814+AzurLab@users.noreply.github.com>
2020-11-18 17:19:30 +0800

    restore from any port or secret

--- module/src/main/raw/magisk/core/mode.d/redirect/on-start.sh
+++ module/src/main/raw/magisk/core/mode.d/redirect/on-start.sh
@@ -14,22 +14,47 @@
 # - PROXY_BLACKLIST_UID: blacklist uid
 #
 
+conf_file="/storage/emulated/0/Android/data/com.github.kr328.clashm/config.yaml"
 selector_file="/storage/emulated/0/Android/data/com.github.kr328.clashm/selector.txt"
 
+get_ec_parameter() {
+     clash_ec_port=$(cat ${conf_file} | grep -E "^external-controller:" | head -n 1 | sed -E 's/external-controller: .*:([0-9]*).*/\1/')
+     clash_secret=$(cat ${conf_file} | grep -E "^secret:" | head -n 1 | sed -E 's/secret: "?([^"]*)"?.*/\1/')
+     if [ "${clash_ec_port}" = "" ];then
+         clash_ec_port="9090"
+     fi
+     if [ "${clash_secret}" = "" ];then
+         clash_secret=""
+     fi
+}
+
 selector_restore() {
-va="0"
-while read line
-do
-    if [ "$va" = "0" ];
+    get_ec_parameter
+    va="0"
+    while read line
+    do
+        if [ "$va" = "0" ];
+        then
+            va="1"
+            group=$(echo $line |tr -d '\n' |od -An -tx1|tr ' ' %|tr -d '\n')
+        else
+            va="0"
+            selector=$line
+            curl -v -H "Authorization: Bearer ${clash_secret}" -X PUT -d "{${selector}}" "127.0.0.1:${clash_ec_port}/proxies/${group}"
+        fi
+    done < ${selector_file}
+}
+
+selector_record() {
+    get_ec_parameter
+    curl -H "Authorization: Bearer ${clash_secret}" http://127.0.0.1:${clash_ec_port}/proxies | sed -E 's/Selector/Selector\n/g' | sed '$d' | sed -E 's/.*name":"(.*)","now":"(.*)","type.*/\1\n"name":"\2"/' > ${selector_tmp}
+    if test -s ${selector_tmp};
     then
-      va="1"
-      group=$(echo $line |tr -d '\n' |od -An -tx1|tr ' ' %|tr -d '\n')
+        cp -f ${selector_tmp} ${selector_file}
     else
-      va="0"
-      selector=$line
-      curl -v -X PUT -d "{${selector}}" "127.0.0.1:9090/proxies/${group}"
+        echo "Selector empty, selector.txt not updated"
     fi
-done < ${selector_file}
+    rm -f ${selector_tmp}
 }
 
 assert() {
@@ -107,4 +132,3 @@ assert iptables -t nat -I OUTPUT -p udp -j CLASH_DNS_LOCAL
 assert iptables -t nat -I PREROUTING -p udp -j CLASH_DNS_EXTERNAL
 
 selector_restore
-
